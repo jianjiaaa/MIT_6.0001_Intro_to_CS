0a6315cfe02c879cfa86f4b887dfb63f8cba98af
unknown <49296983+jianjiaaa@users.noreply.github.com>
2019-05-18 00:10:15 +0200

    Complete ps0

---
+++ ps0_answer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 21:02:54 2019
+
+@author: user
+"""
+import numpy as np
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("X**y = {}".format(x**y))
+print("log(x) = {}".format(np.log2(x)))
